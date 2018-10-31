3b3124fd389b1ef975c0950bffc47bf3258d68e2
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-10-31 16:35:20 +0100

    done cookies section, changed favicons

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ const menuButton = document.querySelector('.button-box');
 menuButton.addEventListener('click', () => {
     menu.classList.toggle('nav--active');
 })
+
+if(localStorage.getItem('isCookiesAccepted', 'true')){
+    document.getElementById('cookies').classList.add('cookies__hidden');
+}
+
+function buttonClick(){
+    localStorage.setItem('isCookiesAccepted', 'true');
+    document.getElementById('cookies').classList.add('cookies__hidden');
+ }
+
+document.getElementById('cookies__button').addEventListener('click', buttonClick);
