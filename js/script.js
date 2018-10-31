1df39a7c82d4d49478757d31e8832cb026eddc7b
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-10-31 16:54:59 +0100

    Add basic styles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
+// Toggling menu
+
 const menu = document.querySelector('.nav');
 const menuButton = document.querySelector('.button-box');
 
 menuButton.addEventListener('click', () => {
     menu.classList.toggle('nav--active');
 })
+
+// Price calculating functionality
+
+const price = document.querySelector('.section-price__value')
+
+const range = document.querySelector('.section-price__range');
+const checkboxes = document.querySelectorAll('.section-price__checkbox')
+const free = document.getElementById('free');
+
+if (free.checked === true) {
+    range.addEventListener('change', () => {
+        price.textContent = range.value;
+    });
+} 
+
+
+
