a27cb2534ae1856d7e90fb4527c0032e8c9e18fa
Krystian Kamiński <krystiankaminski07@gmail.com>
2018-11-01 17:12:31 +0100

    fixed cookies

--- js/script.js
+++ js/script.js
@@ -7,6 +7,19 @@ menuButton.addEventListener('click', () => {
     menu.classList.toggle('nav--active');
 })
 
+// Cookies
+
+if (localStorage.getItem('isCookiesAccepted', 'true')) {
+    document.getElementById('cookies').classList.add('cookies__hidden');
+}
+
+function buttonClick() {
+    localStorage.setItem('isCookiesAccepted', 'true');
+    document.getElementById('cookies').classList.add('cookies__hidden');
+}
+
+document.getElementById('cookies__button').addEventListener('click', buttonClick);
+
 // Price calculating functionality
 
 const price = document.querySelector('.section-price__price')
