ae003c838ecbea95b1b2ad8db66a760ebd052b84
KrystianPietroń <krystianpietr@gmail.com>
2018-11-03 01:09:19 +0100

    chnge carousel.js and style.css

--- js/carousel.js
+++ js/carousel.js
@@ -2,17 +2,29 @@
   var container = document.querySelector("#hero");
   var hero = container.querySelectorAll("#item");
   function change() {
+    document.getElementById("hero").onmouseenter = function() {
+      mouseEnter();
+    };
+    document.getElementById("hero").onmouseleave = function() {
+      mouseLeave();
+    };
     var current = container.querySelector(`#item.current`);
     var next = container.querySelector(`#item.current + #item`) || hero[0];
-
     if (current) {
       current.classList.remove("current");
-    }
 
+    }
     if (next) {
       next.classList.add("current");
     }
   }
   change();
-  setInterval(change, 15000);
+  function mouseLeave() {
+    this.time = setInterval(change, 5000);
+    console.log(`start`);
+  }
+  function mouseEnter() {
+    clearInterval(this.time);
+    console.log(`stop`);
+  }
 })();
