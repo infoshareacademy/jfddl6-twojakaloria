147061a8ea37fe3456c32a7e8151d6af4f37014a
Krystian Pietroń <krystianpietr@gmail.com>
2018-11-07 18:32:17 +0100

    set a interval for game box

--- js/game.js
+++ js/game.js
@@ -4,11 +4,11 @@ class Game {
         this.container = container
         this.items = [{
             x: 470,
-            y: 0
+            y: -5
         },
         {
             x: 220,
-            y: 0
+            y: -5
         }]
 
         this.init()
@@ -24,23 +24,20 @@ class Game {
         this.container.style.width =  '500px'
 
         this.items.forEach( (item) => {
-            this.renderItem(item)
+            setInterval(() => this.renderItem(item), 100)
         })
 
     }
-
     renderItem(item) {
+       
         const itemDiv = document.createElement('div')
         itemDiv.style.height = `30px`
         itemDiv.style.width = `30px`
         itemDiv.style.position = 'absolute'
-        itemDiv.style.top = `${item.y = item.y + 5}px`
         itemDiv.style.left = item.x + 'px'
         itemDiv.style.backgroundColor = 'black'
         this.container.appendChild(itemDiv)
-        item.y = item.y
-        setInterval( () => this.renderItem(item), 500)
-
+        itemDiv.style.top = `${item.y = item.y +5}px`
     }
     itemMove() {
 
