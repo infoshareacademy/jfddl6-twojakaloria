c3a25c42638ac4338112aad769d57443fdec88e4
Krystian Kamiński <krystiankaminski07@gmail.com>
2018-11-08 22:10:34 +0100

    random elements

--- js/game.js
+++ js/game.js
@@ -1,49 +1,49 @@
 class Game {
-    constructor(container) {
-        this.container = container
-        this.items = [{
-            x: 200,
-            y: 0
-        },
-        {
-            x: 299,
-            y: 0
-        }
-    ]
-    this.speed = 5
+  constructor(container) {
+    this.container = container;
+    this.items = [];
+    this.speed = 5;
 
-        this.init()
+    this.init();
+  }
+  itemPosition() {
+    for (let i = 0; i < 5; i++) {
+      this.items.push({
+        x: Math.floor(Math.random() * 300),
+        y: -5
+      });
     }
-    init() {
-        this.render()
-    }
-    render() {
-        this.container.innerHTML = ''
+  }
 
-        this.container.style.backgroundColor = 'red'
-        this.container.style.height = '500px'
-        this.container.style.width = '500px'
+  init() {
+    this.render();
+    this.itemPosition();
+    this.render();
+  }
+  render() {
+    this.container.innerHTML = "";
 
-        this.items.forEach((item) => {
-            this.renderItem(item)
-        })
+    this.container.style.backgroundColor = "red";
+    this.container.style.height = "500px";
+    this.container.style.width = "500px";
 
-    }
+    this.items.forEach(item => {
+      this.renderItem(item);
+    });
+  }
 
-    renderItem(item) {
-
-        const itemDiv = document.createElement('div')
-        itemDiv.style.height = `30px`
-        itemDiv.style.width = `30px`
-        itemDiv.style.position = 'absolute'
-        itemDiv.style.left = item.x + 'px'
-        itemDiv.style.backgroundColor = 'black'
-        this.container.appendChild(itemDiv)
-        itemDiv.style.top = item.y + 'px'
-    }
+  renderItem(item) {
+    const itemDiv = document.createElement("div");
+    itemDiv.style.height = `30px`;
+    itemDiv.style.width = `30px`;
+    itemDiv.style.position = "absolute";
+    itemDiv.style.left = item.x + "px";
+    itemDiv.style.backgroundColor = "black";
+    this.container.appendChild(itemDiv);
+    itemDiv.style.top = item.y + "px";
+  }
 }
 
+const ourContainer = document.querySelector(".game-container");
 
-const ourContainer = document.querySelector('.game-container')
-
-const game = new Game(ourContainer)
+const game = new Game(ourContainer);
